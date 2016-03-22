e56714b9a7e59fbd0183fa573bfeb3de4db565db
Ben Brunton <benbrunton@hotmail.com>
2016-03-22 22:59:28 +0000

    couple of visual updates

--- src/game.rs
+++ src/game.rs
@@ -28,9 +28,9 @@ impl Game{
     }
 
     pub fn start(&mut self){
-        println!("\n\t--------------------");
-        println!("\tWelcome to solitaire!");
-        println!("\t--------------------\n");
+        println!("\n\t\t--------------------");
+        println!("{}", "\t\tWelcome to Solitaire!".bold());
+        println!("\t\t--------------------\n");
         
         
         // deal cards
@@ -39,7 +39,6 @@ impl Game{
         
         while stacks_to_deal > 0 {
             for n in 1..(stacks_to_deal + 1){
-                println!("{}", n);
                 let card = self.deck.take(1);
                 self.tableau[tableau_len - n].add_to_top(card);
             }
@@ -61,7 +60,7 @@ impl Game{
     
     fn display_board(&self){
         // deck
-        let deck = "[🂠]".blue();
+        let deck = "?".blue();
         
         // stack
         let stack = if self.stack.count() > 0 {
@@ -76,25 +75,21 @@ impl Game{
             if stack.count() > 0 {
                 stack.top_card().to_string()
             } else {
-                String::from("🂠")
+                String::from(" ")
             }
         ).collect::<Vec<String>>();
         
-        println!("[h]\t[s]\t\t[a]\t[b]\t[c]\t[d]");
+        println!("<h>\t<s>\t\t\t<a>\t<b>\t<c>\t<d>");
         
-        println!("{}\t{}\t\t[{}]\t[{}]\t[{}]\t[{}]", deck, stack, target[0], target[1], target[2], target[3]);
+        println!("[{}]\t{}\t\t\t[{}]\t[{}]\t[{}]\t[{}]", deck, stack, target[0], target[1], target[2], target[3]);
         
         let stacks = &self.tableau.iter().map(|stack|
-            if stack.count() > 0 {
-                stack.count().to_string().blue()
-            } else {
-                "🂠".normal()
-            }
+            stack.count().to_string().blue()
         ).collect::<Vec<ColoredString>>();
         
         
         // stacks
-      println!("\n\t[1]\t[2]\t[3]\t[4]\t[5]\t[6]\t[7]");
+      println!("\n\t<1>\t<2>\t<3>\t<4>\t<5>\t<6>\t<7>");
         println!("\t[{}]\t[{}]\t[{}]\t[{}]\t[{}]\t[{}]\t[{}]", stacks[0], stacks[1], stacks[2], stacks[3],
             stacks[4], stacks[5], stacks[6]);
     }
